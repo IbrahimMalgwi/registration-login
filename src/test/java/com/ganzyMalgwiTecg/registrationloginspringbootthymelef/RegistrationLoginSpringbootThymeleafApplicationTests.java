b3538677b253b7ff3c0adcbd70201f60a2dbcb44
IbrahimGanaMalgwi <ibrahimganamalgwi@outlook.com>
2023-02-07 01:01:51 +0100

    first commit

---
+++ src/test/java/com/ganzyMalgwiTecg/registrationloginspringbootthymelef/RegistrationLoginSpringbootThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganzyMalgwiTecg.registrationloginspringbootthymelef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationLoginSpringbootThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
